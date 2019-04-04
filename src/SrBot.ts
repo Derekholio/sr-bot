e97634257c7122db1a75ef04c809f6d4af0f44ef
ryanpetersonOF <ryan@openfin.co>
2019-04-04 10:58:21 +0100

    Add command base work

--- src/SrBot.ts
+++ src/SrBot.ts
@@ -1,10 +1,18 @@
 import * as Discord from 'discord.js';
 import {getRankEmoji} from './utils/getRankEmoji';
-import {DiscordConfig, ConfigurationLoc} from './types';
+import {DiscordConfig, ConfigurationLoc, Server} from './types';
 import {getJsonFile} from './utils/getJsonFile';
 import {StatsGenerator} from './StatsGenerator';
 import {log} from './utils/logger';
-import {calculateAverageSR} from './calculateAverageSr';
+import {calculateAverageSR} from './utils/calculateAverageSr';
+
+/**
+ * Enum of available commands
+ */
+enum COMMAND {
+    BASE = '!SR',
+    TEAM = 'TEAM'
+}
 
 /**
  * Defines the Hex color used in the Discord embed message
@@ -48,11 +56,11 @@ export class SrBot {
      * @param message The message received
      */
     private onClientMessageHandler(message: Discord.Message): void {
-        const isUserBot = message.author.bot;
-
-        if (!isUserBot && message.content.toLowerCase().startsWith('!sr')) {
-            this.processTextChat(message);
+        if (message.author.bot || !message.content.toUpperCase().startsWith(COMMAND.BASE)) {
+            return;
         }
+
+        this.commandHandler(message);
     }
 
     /**
@@ -60,44 +68,73 @@ export class SrBot {
      * @param {*} message
      */
     private processTextChat(message: Discord.Message): void {
-        if (message.content.toLowerCase() === '!sr') {
-            log('CLIENT', `New request from ${message.author.username}`);
-            const serverId = message.member.guild.id;
-            const requestedServer = this.statsGenerator.getLastResult().servers.find(server => serverId === server.id);
+        log('CLIENT', `New request from ${message.author.username}`);
+        const serverId = message.member.guild.id;
+        const requestedServer = this.statsGenerator.getServer(serverId);
 
-            if (!requestedServer) {
-                message.channel.send('Sorry, this server has no players associated.');
-                return;
-            } else {
-                const players = requestedServer.players;
+        if (!requestedServer) {
+            message.channel.send('Sorry, this server has no players associated.');
+            return;
+        } else {
+            const players = requestedServer.players;
 
-                // Embed Construction
-                const embed = new Discord.RichEmbed()
-                    .setTitle(`Leaderboard: ${requestedServer.teamName}`)
-                    .setColor(HEX_EMBED_COLOR)
-                    .setFooter(`Last updated: ${new Date(this.statsGenerator.getLastResult().timestamp).toUTCString()}`)
-                    .setAuthor('SR Bot', undefined, 'https://github.com/Derekholio/sr-bot');
+            // Embed Construction
+            const embed = new Discord.RichEmbed()
+                .setTitle(`Leaderboard: ${requestedServer.teamName}`)
+                .setColor(HEX_EMBED_COLOR)
+                .setFooter(`Last updated: ${new Date(this.statsGenerator.getLastResult().timestamp).toUTCString()}`)
+                .setAuthor('SR Bot', undefined, 'https://github.com/Derekholio/sr-bot');
 
-                players.forEach((player) => {
-                    embed.addField(player.player, `${player.SR}${player.private ? ' [PRIVATE]' : ''} ${getRankEmoji(player.SR)}`);
-                });
+            players.forEach((player) => {
+                embed.addField(player.player, `${player.SR}${player.private ? ' [PRIVATE]' : ''} ${getRankEmoji(player.SR)}`);
+            });
 
-                // Team Stats Construction
-                const average = calculateAverageSR(players);
-                let teamStatsMessage = `Average SR: ${average}`;
+            // Team Stats Construction
+            const average = calculateAverageSR(players);
+            let teamStatsMessage = `Average SR: ${average}`;
 
-                if (requestedServer.targetSR) {
-                    const playersCount = players.length;
-                    const target = requestedServer.targetSR;
-                    const max = Math.abs((average * playersCount) - (target * (playersCount + 1)));
-                    teamStatsMessage += `\nTarget SR: ${target}\nMax add: ${max}`;
-                }
-                embed.addField('Team Stats', teamStatsMessage);
-
-                // Send message to discord
-                log('CLIENT', `Sent embed to chat: ${JSON.stringify(embed)}`);
-                message.channel.send({embed});
+            if (requestedServer.targetSR) {
+                const playersCount = players.length;
+                const target = requestedServer.targetSR;
+                const max = Math.abs((average * playersCount) - (target * (playersCount + 1)));
+                teamStatsMessage += `\nTarget SR: ${target}\nMax add: ${max}`;
             }
+            embed.addField('Team Stats', teamStatsMessage);
+
+            // Send message to discord
+            log('CLIENT', `Sent embed to chat: ${JSON.stringify(embed)}`);
+            message.channel.send({embed});
+        }
+    }
+
+    /**
+     * Handles commands sent from discord
+     * @param message Incoming discord message
+     */
+    private commandHandler(message: Discord.Message): void {
+        const serverId = message.member.guild.id;
+        const server = this.statsGenerator.getServer(serverId);
+        const [command, ...params] = [...message.content.split(' ').slice(1)];
+
+        if (!command) {
+            this.processTextChat(message);
+            return;
+        }
+
+        switch (command.toUpperCase()) {
+            case COMMAND.TEAM:
+                if (params[0]) {
+                    const teamName = params.join(' ');
+                    this.statsGenerator.updateServerProperty(serverId, 'teamName', teamName);
+                    message.channel.send(`Team name updated: ${teamName}`);
+                } else {
+                    message.channel.send(`Team name: ${server && server.teamName || 'NOT SET'}`);
+                }
+                break;
+
+            default:
+                this.processTextChat(message);
+                break;
         }
     }
 }
